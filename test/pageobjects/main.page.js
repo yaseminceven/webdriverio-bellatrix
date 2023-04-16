9bc41de5ab0f615acd559184a4e26cfbf269550a
yaseminceven <yaseminzeynep97@gmail.com>
2023-04-16 15:07:34 +0300

    added sorting case for homepage

--- test/pageobjects/main.page.js
+++ test/pageobjects/main.page.js
@@ -1,4 +1,5 @@
 const Page = require('./page');
+const assert = require('assert');
 
 class MainPage extends Page {
     get dropdown () {
@@ -13,6 +14,10 @@ class MainPage extends Page {
         return $('.products.columns-4').$$('li')[`${productIndex}`].$('a[href*=add-to-cart]');
     }
 
+    productPrice (){
+        return $$("span > ins:nth-child(2) > span:nth-child(1) > bdi");
+    } 
+
     async selectSortingOption (text) {
         await this.dropdown.waitForDisplayed();
         await this.dropdown.selectByVisibleText(text);
@@ -20,6 +25,21 @@ class MainPage extends Page {
 
     async clickAddToCart (productIndex) {
         await this.addToCartButton(productIndex).click();
+    }
+
+    async checkPriceAscending() {
+        const priceList = await this.productPrice();
+        const newPriceValues = await Promise.all(priceList.map(async price => {
+            const priceText = await price.getText();
+            if (priceText) {
+              return parseFloat(priceText.replace(/[^0-9.-]+/g,""));
+            } else {
+              return null;
+            }
+          }));  
+        const newValidPriceValues = newPriceValues.filter(price => price !== null);  
+        const sortedValues = newValidPriceValues.slice().sort(function(a, b){return a - b});
+        assert.deepEqual(sortedValues,newPriceValues);
     }
 
     open () {
--- test/specs/homepage.spec.js
+++ test/specs/homepage.spec.js
@@ -1,5 +1,6 @@
 const MainPage = require('../pageobjects/main.page');
 
+
 describe('Homepage test cases', async () => {
 
     it('add product to cart at homepage', async () => {
@@ -7,5 +8,11 @@ describe('Homepage test cases', async () => {
         await MainPage.selectSortingOption('Sort by popularity');
         await MainPage.clickAddToCart(0);
         await expect(MainPage.cartItemsText).toHaveTextContaining('1');
+    })
+
+    it('sort by price low to high at homepage',async () => {
+        await MainPage.open();
+        await MainPage.selectSortingOption('Sort by price: low to high');
+        await MainPage.checkPriceAscending();
     })
 });
